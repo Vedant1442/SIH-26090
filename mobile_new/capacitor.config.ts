ecb353404f6356c66e69cfa104e333c959090113
Vedant1442 <vedanttemp9@gmail.com>
2026-09-03 01:40:34 +0530

    Update requirements and capacitor config

--- mobile_new/capacitor.config.ts
+++ mobile_new/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.sih.kalasangam',
   appName: 'Kalasangam',
-  webDir: 'dist'
+  webDir: '.output/public'
 };
 
 export default config;
